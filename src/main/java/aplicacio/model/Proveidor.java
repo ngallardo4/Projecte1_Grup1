1b62570a37cfc6fe3406432bdd98863430b9bd4b
Daniel <dcorral@alumnat.copernic.cat>
2024-10-01 12:32:06 +0200

    atributos y constructor

--- src/main/java/aplicacio/model/Proveidor.java
+++ src/main/java/aplicacio/model/Proveidor.java
@@ -4,10 +4,30 @@
  */
 package aplicacio.model;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author ngall
  */
 public class Proveidor {
-    
-}
+        public String CIF;
+    public String Nom;
+    public String MotiuInactiu;
+    public String Telefon;
+    public float Descompte;
+    public LocalDate Data_Alt;
+    public int Qualificacio;
+
+    public Proveidor(String CIF, String Nom, String MotiuInactiu, String Telefon, 
+                     float Descompte, LocalDate Data_Alt, int Qualificacio) {
+        this.CIF = CIF;
+        this.Nom = Nom;
+        this.MotiuInactiu = MotiuInactiu;
+        this.Telefon = Telefon;
+        this.Descompte = Descompte;
+        this.Data_Alt = Data_Alt;
+        this.Qualificacio = Qualificacio;
+        }
+
+    }
