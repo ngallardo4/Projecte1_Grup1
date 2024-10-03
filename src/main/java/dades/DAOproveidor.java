4a9c6b1c689d64cbb15b0fd34d0fa11ac575679a
Daniel <dcorral@alumnat.copernic.cat>
2024-10-03 08:50:42 +0200

    metode modificar proveidor

--- src/main/java/dades/DAOproveidor.java
+++ src/main/java/dades/DAOproveidor.java
@@ -23,6 +23,7 @@ public class DAOproveidor implements DAOinterface<Proveidor> {
         try(Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/projecte1?useUnicode=true&serverTimezone=Europe/Madrid", "root", "123456")){
             PreparedStatement stmt = conn.prepareStatement("INSERT INTO proveidor (CIF, Nom, Estat, MotiuInactiu, Telefon, Descompte, Data_Alt, Qualificacio) VALUES " + "(?, ?, ?, ?, ?, ?, ?)\";");
             
+            
             stmt.setString  (1, proveidor.CIF);
             stmt.setString  (2, proveidor.Nom);
             stmt.setString  (3, proveidor.getEstat().name());
@@ -30,7 +31,7 @@ public class DAOproveidor implements DAOinterface<Proveidor> {
             stmt.setString  (5, proveidor.Telefon);
             stmt.setFloat   (6, proveidor.Descompte);
             //La línea asegura que el valor de fecha se inserte correctamente en la base de datos, 
-            //utilizando el tipo adecuado para evitar errores y garantizar el uso correcte de datos de fecha.
+            //utilizando el tipo adecuado para evitar errores y garantizar el uso correcto de datos de fecha.
             stmt.setDate    (7, java.sql.Date.valueOf(proveidor.getData_Alt()));
             stmt.setInt     (8, proveidor.Qualificacio);
             
@@ -43,8 +44,8 @@ public class DAOproveidor implements DAOinterface<Proveidor> {
     }
 
     @Override
-    public void actualitzar(Proveidor entitat) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void actualitzar(Proveidor proveidor) {
+        
     }
 
     @Override
