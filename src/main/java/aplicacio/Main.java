ae8415830636022dc747b40e00a3126913b21f52
nacho <ngallardo@alumnat.copernic.cat>
2024-10-04 10:48:05 +0200

    prueba para solucionar el error HEAD al hacer el pull

--- src/main/java/aplicacio/Main.java
+++ src/main/java/aplicacio/Main.java
@@ -16,6 +16,7 @@ public class Main {
         String rutaArxiu = "uspass.txt";
         
         DAOusuariImpl usuariDAO = new DAOusuariImpl(rutaArxiu);
+        System.out.println("");
     }
     
 }
