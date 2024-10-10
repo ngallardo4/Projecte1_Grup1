989f2123e09d3fb5d7b4811f4c4c1441cd9c2103
nacho <ngallardo@alumnat.copernic.cat>
2024-10-10 22:11:38 +0200

    Afegit el javadoc de familia

--- src/main/java/aplicacio/model/Familia.java
+++ src/main/java/aplicacio/model/Familia.java
@@ -7,10 +7,11 @@ package aplicacio.model;
 import java.time.LocalDate;
 
 /**
- * Classe que representa una familia al sistema.
- * Proporciona informació sobre la família, incluent el seu nom, descripció i altres propietats.
- * 
+ * descripció: Aquesta classe representa una familia del sistema amb els atributs
+ * id, el nom, la descripció, la data d'alta, el proveïdor per defecte i les observacions.
+ *
  * @author Yaiza
+ * @version 10/2024.1
  */
 public class Familia {
 
@@ -22,8 +23,13 @@ public class Familia {
     private String observacions;
 
     /**
-     * Constructor de la classe {@code Familia}.
-     * 
+     * Constructor per inicialitzar una nova família amb els detalls proporcionats.
+     * @param id, Identificador únic de la família.
+     * @param nom, Nom de la família.
+     * @param descripcio, Descripció de la família.
+     * @param data_alta, Data d'alta de la família en el sistema.
+     * @param prov_defecte, Proveïdor per defecte de la família.
+     * @param observacions, Observacions addicionals sobre la família.
      */
     public Familia(int id, String nom, String descripcio, LocalDate data_alta, String prov_defecte, String observacions) {
         this.id = id;
@@ -34,50 +40,92 @@ public class Familia {
         this.observacions = observacions;
     }
 
+    /**
+     * @return L'identificador de la família.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Estableix l'identificador de la família.
+     * @param id El nou identificador de la família.
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * @return El nom de la família.
+     */
     public String getNom() {
         return nom;
     }
 
+    /**
+     * Estableix el nom de la família.
+     * @param nom El nou nom de la família.
+     */
     public void setNom(String nom) {
         this.nom = nom;
     }
 
+    /**
+     * @return La descripció de la família.
+     */
     public String getDescripcio() {
         return descripcio;
     }
 
+    /**
+     * Estableix la descripció de la família.
+     * @param descripcio La nova descripció.
+     */
     public void setDescripcio(String descripcio) {
         this.descripcio = descripcio;
     }
 
+    /**
+     * @return La data d'alta de la família.
+     */
     public LocalDate getData_alta() {
         return data_alta;
     }
 
+    /**
+     * Estableix la data d'alta de la família al sistema.
+     * @param data_alta la nova data d'alta.
+     */
     public void setData_alta(LocalDate data_alta) {
         this.data_alta = data_alta;
     }
 
+    /**
+     * @return El proveïdor per defecte de la família.
+     */
     public String getProv_defecte() {
         return prov_defecte;
     }
 
+    /**
+     * Estableix el proveïdor per defecte de la família.
+     * @param prov_defecte El nou proveïdor per defecte.
+     */
     public void setProv_defecte(String prov_defecte) {
         this.prov_defecte = prov_defecte;
     }
 
+    /**
+     * @return Les observacions de la família.
+     */
     public String getObservacions() {
         return observacions;
     }
 
+    /**
+     * Estableix les observacions de la família.
+     * @param observacions Les noves observacions de la família.
+     */
     public void setObservacions(String observacions) {
         this.observacions = observacions;
     }
