8178a5db0e66c07966e35ef3414457cba0ea89ac
nacho <ngallardo@alumnat.copernic.cat>
2024-10-10 19:52:51 +0200

    Afegit el javadoc de proveidor

--- src/main/java/aplicacio/model/Proveidor.java
+++ src/main/java/aplicacio/model/Proveidor.java
@@ -8,11 +8,12 @@ import enums.EstatProveidor;
 import java.time.LocalDate;
 
 /**
- * Clase que representa un proveedor en el sistema.
- * Contiene información sobre el proveedor como su CIF, nombre, estado,
- * motivo de inactividad, teléfono, descuento, fecha de alta y calificación.
+ * descripció: Aquesta classe representa un proveïdor al sistema.
+ * Conté informació sobre el proveïdor com el seu CIF, nom, estat (Actiu o Inactiu),
+ * motiu d'inactivitat, número de telèfon, descompte, data d'alta i qualificació.
  * 
  * @author ngall
+ * @version 10/2024.1
  */
 public class Proveidor {
     private String CIF;
@@ -24,11 +25,17 @@ public class Proveidor {
     private LocalDate Data_Alta;
     private int Qualificacio;
 
-     /**
-     * Constructor para crear un nuevo proveedor con todos los campos.
-     *
+    /**
+     * Constructor per crear un nou proveïdor amb els detalls proporcionats.
+     * @param CIF, identificador únic per cada proveïdor.
+     * @param Nom, el nom del proveïdor.
+     * @param Estat, l'estat en què es troba el proveïdor. (Actiu o Inactiu)
+     * @param MotiuInactiu, si l'estat es inactiu detallarem en aquest paràmetre el motiu.
+     * @param Telefon, el telèfon del proveïdor.
+     * @param Descompte, el descompte del proveïdor.
+     * @param Data_Alta, la data d'alta del proveïdor al sistema.
+     * @param Qualificacio, la qualificació general que té el proveïdor.
      */
-    
     public Proveidor(String CIF, String Nom, EstatProveidor Estat, String MotiuInactiu, String Telefon, float Descompte, LocalDate Data_Alta, int Qualificacio) {
         this.CIF = CIF;
         this.Nom = Nom;
@@ -41,75 +48,129 @@ public class Proveidor {
     }
     
     /**
-     * Constructor para crear un nuevo proveedor con solo CIF y nombre.
-     *
-     * 
-     * @param Nom El nombre del proveedor.
+     * Constructor per crear un nou proveïdor només amb el nom.
+     * @param Nom El nom del proveïdor.
      */
     public Proveidor(String Nom) {
         this.Nom = Nom;
     }
     
+    /**
+     * @return el CIF del proveïdor.
+     */
     public String getCIF() {
         return CIF;
     }
 
+    /**
+     * Estableix el CIF del proveïdor.
+     * @param CIF, l'identificador únic del proveïdor.
+     */
     public void setCIF(String CIF) {
         this.CIF = CIF;
     }
 
+    /**
+     * @return el nom del proveïdor.
+     */
     public String getNom() {
         return Nom;
     }
 
+    /**
+     * Estableix el nom del proveïdor.
+     * @param Nom, el nom del proveïdor.
+     */
     public void setNom(String Nom) {
         this.Nom = Nom;
     }
 
+    /**
+     * @return l'estat del proveïdor (Actiu o Inactiu).
+     */
     public EstatProveidor getEstat() {
         return Estat;
     }
 
+    /**
+     * Estableix l'estat del proveïdor
+     * @param Estat, l'estat del proveïdor, pot ser: Actiu o inactiu.
+     */
     public void setEstat(EstatProveidor Estat) {
         this.Estat = Estat;
     }
     
+    /**
+     * @return el motiu d'inactivitat del proveïdor.
+     */
     public String getMotiuInactiu() {
         return MotiuInactiu;
     }
 
+    /**
+     * Estableix el motiu d'inactivitat del proveïdor.
+     * @param MotiuInactiu, el motiu pel qual el proveïdor està inactiu.
+     */
     public void setMotiuInactiu(String MotiuInactiu) {
         this.MotiuInactiu = MotiuInactiu;
     }
 
+    /**
+     * @return el telèfon del proveïdor.
+     */
     public String getTelefon() {
         return Telefon;
     }
 
+    /**
+     * Estableix el telèfon del proveïdor.
+     * @param Telefon, el telèfon del proveïdor.
+     */
     public void setTelefon(String Telefon) {
         this.Telefon = Telefon;
     }
 
+    /**
+     * @return el descompte del proveïdor.
+     */
     public float getDescompte() {
         return Descompte;
     }
 
+    /**
+     * Estableix el descompte del proveïdor.
+     * @param Descompte, el descompte del proveïdor.
+     */
     public void setDescompte(float Descompte) {
         this.Descompte = Descompte;
     }
 
+    /**
+     * @return la data d'alta del proveïdor al sistema.
+     */
     public LocalDate getData_Alta() {
         return Data_Alta;
     }
 
+    /**
+     * Estableix la data d'alta del proveïdor al sistema.
+     * @param Data_Alta, la data d'alta del proveïdor.
+     */
     public void setData_Alta(LocalDate Data_Alta) {
         this.Data_Alta = Data_Alta;
     }
 
+    /**
+     * @return la qualificació general del proveïdor.
+     */
     public int getQualificacio() {
         return Qualificacio;
     }
 
+    /**
+     * Estableix la qualificació general del proveïdor.
+     * @param Qualificacio, la qualificació del proveïdor.
+     */
     public void setQualificacio(int Qualificacio) {
         this.Qualificacio = Qualificacio;
     }
